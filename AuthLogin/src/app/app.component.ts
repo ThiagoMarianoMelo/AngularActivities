3e82e75c2cd43bb7e5586fecd749eb9d43b01939
ThiagoMelo <thiago.17mel@gmail.com>
2023-01-15 11:39:10 -0300

    Desenvolvimento Saas sign page

--- AuthLogin/src/app/app.component.ts
+++ AuthLogin/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<a [routerLink] = "['admin']">Admin</a>
+  <br>
+  <a [routerLink] = "['']">Sign</a>
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'AuthLogin';
